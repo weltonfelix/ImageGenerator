1b92dda18b19400aa726bf2baaba4f5b5bdb176b
Welton Felix <contato.weltonf@gmail.com>
2021-03-01 13:18:52 -0300

    Fix production browser

--- util/renderFile.ts
+++ util/renderFile.ts
@@ -1,11 +1,12 @@
-import { puppeteer } from "chrome-aws-lambda";
-import type { Page } from 'puppeteer';
+import puppeteer from "puppeteer";
+import chrome from 'chrome-aws-lambda';
 
 export enum fileFormats {
   'jpeg',
   'png',
   'pdf',
 }
+
 interface renderFileProps {
   htmlContent: string;
   fileFormat: keyof typeof fileFormats;
@@ -13,13 +14,13 @@ interface renderFileProps {
 
 export default async function({ htmlContent, fileFormat }: renderFileProps) {
   const fileFormatsHandlers = {
-    'jpeg': async (page: Page) => {
+    'jpeg': async (page: puppeteer.Page) => {
       return await page.screenshot({ type: 'jpeg' })
     },
-    'png': async (page: Page) => {
+    'png': async (page: puppeteer.Page) => {
       return await page.screenshot({ type: 'png' })
     },
-    'pdf': async (page: Page) => {
+    'pdf': async (page: puppeteer.Page) => {
       return await page.pdf({
         format: 'a4',
         printBackground: true,
@@ -27,7 +28,20 @@ export default async function({ htmlContent, fileFormat }: renderFileProps) {
     },
   }
 
-  const browser = await puppeteer.launch();
+  const isDev = !process.env.AWS_REGION;
+
+  let browserOptions = null;
+
+  if (isDev) {
+    var browser = await puppeteer.launch();
+  } else {
+    browserOptions = {
+      args: chrome.args,
+      executablePath: await chrome.executablePath,
+    };
+
+    var browser = await puppeteer.launch(browserOptions);
+  }
 
   const page = await browser.newPage();
   await page.setJavaScriptEnabled(false);
