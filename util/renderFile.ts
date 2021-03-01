364e966337a6d790951f79050f2c0f956d545a48
Welton Felix <contato.weltonf@gmail.com>
2021-03-01 13:00:56 -0300

    Fix Page type import

--- util/renderFile.ts
+++ util/renderFile.ts
@@ -1,11 +1,11 @@
 import { puppeteer } from "chrome-aws-lambda";
+import type { Page } from 'puppeteer';
 
 export enum fileFormats {
   'jpeg',
   'png',
   'pdf',
 }
-
 interface renderFileProps {
   htmlContent: string;
   fileFormat: keyof typeof fileFormats;
@@ -13,13 +13,13 @@ interface renderFileProps {
 
 export default async function({ htmlContent, fileFormat }: renderFileProps) {
   const fileFormatsHandlers = {
-    'jpeg': async (page: puppeteer.Page) => {
+    'jpeg': async (page: Page) => {
       return await page.screenshot({ type: 'jpeg' })
     },
-    'png': async (page: puppeteer.Page) => {
+    'png': async (page: Page) => {
       return await page.screenshot({ type: 'png' })
     },
-    'pdf': async (page: puppeteer.Page) => {
+    'pdf': async (page: Page) => {
       return await page.pdf({
         format: 'a4',
         printBackground: true,
